16d7550c8b52a69a72cd3e68298b7774eab44130
José Vidal <jvidalf23@fpcoruna.afundacion.org>
2025-11-11 14:21:05 +0100

    ejercicio 2

--- sprint3android/My Catalog/app/src/main/java/com/example/catalogactivity/MainActivity.java
+++ sprint3android/My Catalog/app/src/main/java/com/example/catalogactivity/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.catalogactivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnNavegarDetalle = findViewById(R.id.btn_navegar_detalle);
+        btnNavegarDetalle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Lanzar DetailActivity
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
