c669308fc2dae598164634c2aab0275bb7a8cd90
José Vidal <jvidalf23@fpcoruna.afundacion.org>
2025-11-19 20:30:50 +0100

    Creación metodos abrir ventana y añadir usuario

--- registro_usuarios_ctk/controller/app_controller.py
+++ registro_usuarios_ctk/controller/app_controller.py
@@ -1,6 +1,8 @@
-from model.usuario_model import GestorUsuarios
-from view.main_view import MainView
+from view.main_view import AddUserView, MainView
+from model.usuario_model import Usuario, GestorUsuarios
+from tkinter import messagebox
 from pathlib import Path
+import customtkinter as ctk
 
 class AppController:
     def __init__(self, master):
@@ -15,7 +17,6 @@ class AppController:
 
         self.refrescar_lista_usuarios()
 
-
     def refrescar_lista_usuarios(self):
         usuarios = self.model.listar()
         self.view.actualizar_lista_usuarios(usuarios, self.seleccionar_usuario)
@@ -23,3 +24,30 @@ class AppController:
     def seleccionar_usuario(self, indice):
         usuario = self.model.obtener(indice)
         self.view.mostrar_detalles_usuario(usuario)
+
+    def abrir_ventana_añadir(self):
+        self.add_view = AddUserView(self.master)
+        self.add_view.guardar_button.configure(
+            command=lambda: self.añadir_usuario(self.add_view)
+        )
+
+    def añadir_usuario(self, add_view):
+        datos = add_view.get_data()
+
+        try:
+            edad = int(datos["edad"])
+        except ValueError:
+            messagebox.showerror("Error", "La edad debe ser un número.")
+            return
+
+        nuevo = Usuario(
+            datos["nombre"],
+            edad,
+            datos["genero"],
+            datos["avatar"]
+        )
+
+        self.model._usuarios.append(nuevo)
+        self.refrescar_lista_usuarios()
+        add_view.window.destroy()
+
