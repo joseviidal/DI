29b462aaf2a9c91e0173b78766fa904fd98f41b2
José Vidal <jvidalf23@fpcoruna.afundacion.org>
2025-11-19 20:34:23 +0100

    Creación métodos guardar csv y cargar csv

--- registro_usuarios_ctk/model/usuario_model.py
+++ registro_usuarios_ctk/model/usuario_model.py
@@ -1,3 +1,6 @@
+import csv
+from pathlib import Path
+
 class Usuario:
     def __init__(self, nombre, edad, genero, avatar):
         self.nombre = nombre
@@ -19,3 +22,28 @@ class GestorUsuarios:
 
     def obtener(self, indice):
         return self._usuarios[indice]
+
+    def guardar_csv(self, ruta):
+        with open(ruta, "w", newline="", encoding="utf-8") as archivo:
+            writer = csv.writer(archivo)
+            writer.writerow(["nombre", "edad", "genero", "avatar"])
+
+            for u in self._usuarios:
+                writer.writerow([u.nombre, u.edad, u.genero, u.avatar])
+
+    def cargar_csv(self, ruta):
+        try:
+            with open(ruta, "r", encoding="utf-8") as archivo:
+                reader = csv.reader(archivo)
+                next(reader)
+
+                self._usuarios.clear()
+
+                for fila in reader:
+                    try:
+                        nombre, edad, genero, avatar = fila
+                        self._usuarios.append(Usuario(nombre, int(edad), genero, avatar))
+                    except:
+                        pass
+        except FileNotFoundError:
+            pass
