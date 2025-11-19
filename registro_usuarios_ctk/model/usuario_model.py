863f0d7b0e98bbf9b934f3e385812defe7370ff9
José Vidal <jvidalf23@fpcoruna.afundacion.org>
2025-11-19 10:17:14 +0100

    Creación clase Gestor de usuarios

--- registro_usuarios_ctk/model/usuario_model.py
+++ registro_usuarios_ctk/model/usuario_model.py
@@ -4,3 +4,14 @@ class Usuario:
         self.edad = edad
         self.genero = genero
         self.avatar = avatar
+
+class GestorUsuarios:
+    def __init__ (self):
+        self._usuarios = []
+
+    def _cargar_datos_De_ejemplo(self):
+        self._usuarios.append(Usuario("José Vidal", 20, "Masculino", "avatarJoseVidal.png"))
+        self._usuarios.append(Usuario("Carlitos Mendez", 35, "Masculino", "avatarCarlitosMendez.png"))
+
+    def listar(self):
+        return self._usuarios
