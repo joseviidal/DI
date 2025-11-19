b44bd597919c9fe676e53ed710905e8605c0509f
José Vidal <jvidalf23@fpcoruna.afundacion.org>
2025-11-19 10:40:01 +0100

    Añadir los widgets a MainView

--- registro_usuarios_ctk/view/main_view.py
+++ registro_usuarios_ctk/view/main_view.py
@@ -1,3 +1,6 @@
+import customtkinter as ctk
+import tkinter
+
 class MainView:
     def __init__(self, master):
         self.master = master
@@ -5,3 +8,20 @@ class MainView:
         master.grid_columnconfigure(0, weight=1)
         master.grid_columnconfigure(1, weight=2)
 
+        self.lista_usuarios_scrollable = ctk.CTkScrollableFrame(master)
+        self.lista_usuarios_scrollable.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
+
+        self.detalles_frame = ctk.CTkFrame(master)
+        self.detalles_frame.grid(row=0, column=1, sticky="nsew", padx=10, pady=10)
+
+        self.label_nombre = ctk.CTkLabel(self.detalles_frame, text="Nombre:")
+        self.label_nombre.pack(anchor="w")
+
+        self.label_edad = ctk.CTkLabel(self.detalles_frame, text="Edad:")
+        self.label_edad.pack(anchor="w")
+
+        self.label_genero = ctk.CTkLabel(self.detalles_frame, text="Género:")
+        self.label_genero.pack(anchor="w")
+
+        self.avatar_label = ctk.CTkLabel(self.detalles_frame, text="")
+        self.avatar_label.pack(pady=10)
